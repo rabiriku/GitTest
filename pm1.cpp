7b67795f62fc45b948cf1e974aa71f4319789c79
rabiriku <k022g1093@g.neec.ac.jp>
2022-11-18 09:18:48 +0900

    ローカルコンフリクト変更

--- pm1.cpp
+++ pm1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisialStudioWorld!\n";
 
-    std::cout << " Github Clone chenged\n";
+    std::cout << " Conflict changed\n";
     
     std::cout << "third branch\n";
  
